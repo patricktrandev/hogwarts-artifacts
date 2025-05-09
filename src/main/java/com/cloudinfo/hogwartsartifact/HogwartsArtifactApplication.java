8b17823f2ec7ecc9f48a431bfbb4ac65a333c7d5
patricktrandev <patricktrandev@gmail.com>
2025-05-10 00:09:24 +0700

    (project):init base project

---
+++ src/main/java/com/cloudinfo/hogwartsartifact/HogwartsArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.cloudinfo.hogwartsartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudinfo/hogwartsartifact/HogwartsArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudinfo.hogwartsartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
